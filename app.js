bcb4756904a973e20f83856430f37666622b60f3
Hien Tran <minhhien1772000@gmail.com>
2024-10-30 15:16:50 +0700

    refactor

--- app.js
+++ app.js
@@ -22,4 +22,7 @@ app.use('/api/tasks', taskRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/ai', aiRoutes);
 
+
+
+
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 exports.connect = () => {
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('MongoDB connected'))
+        .then(() => console.log('MongoDB connected', process.env.MONGO_URI))
         .catch(err => console.error('MongoDB connection error:', err));
 };
